3453b0ff59c52d97bfe40817c4c0648f3fc5b14a
LorenzoAblis <lablis228609@gmail.com>
2023-10-24 10:21:13 -0500

    Initial Commit

---
+++ Prospect Athletic Roster/Prospect_Athletic_RosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prospect_Athletic_RosterApp.swift
+//  Prospect Athletic Roster
+//
+//  Created by Lorenzo J. Ablis on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Prospect_Athletic_RosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
